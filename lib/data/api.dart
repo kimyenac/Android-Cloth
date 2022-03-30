0d8bf249df090ae7d0d63014f0686bc41911ddb6
kimyenac <kimyena_c@naver.com>
2022-03-30 15:22:36 +0900

    first upload

---
+++ lib/data/api.dart
@@ -0,0 +1,56 @@
+import 'package:cloth/data/weather.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:collection/collection.dart';
+
+class WeatherApi {
+  final BASE_URL = "http://apis.data.go.kr";
+
+  final String key = "%2FSCJqd%2BrrFbhnUOr9f7WKcoqfDxabnkFYsRJWa92HJc1Bp%2F%2BwT8KUnRMCp46s3u3UdAwse%2B1yVSAL167ePiLjA%3D%3D";
+
+  Future<List<Weather>> getWeather(int x, int y, int date, String base_time) async {
+    String url = "$BASE_URL/1360000/VilageFcstInfoService_2.0/getVilageFcst?"
+        "serviceKey=$key&pageNo=1&numOfRows=130&dataType=json&"
+        "base_date=$date&base_time=$base_time&nx=$x&ny=$y";
+
+    print(url);
+
+    final response = await http.get(url);
+
+    List<Weather> weather = [];
+
+    if(response.statusCode == 200){
+      // 정상으로 진행됨
+      String body = utf8.decode(response.bodyBytes);
+      print(body);
+
+      var res = json.decode(body) as Map<String, dynamic>;
+
+      List<dynamic> _data = [];
+
+      _data = res["response"]["body"]["items"]["item"] as List<dynamic>;
+
+      final data = groupBy(_data, (obj) => "${obj["fcstTime"]}").entries.toList();
+
+      for(final _r in data){
+
+        final _data = {
+          "fcstTime": _r.key,
+          "fcstDate": _r.value.first["fcstDate"]
+        };
+
+        for(final _d in _r.value){
+          _data[_d["category"]] = _d["fcstValue"];
+        }
+
+        final w = Weather.fromJson(_data);
+        weather.add(w);
+      }
+
+      return weather;
+
+    }else{
+      return [];
+    }
+  }
+}
